1b900efaadb225354778db67a0c7a2e6171cd4bd
Kate Han <K.Han3@salford.ac.uk>
2023-10-30 18:19:09 +0000

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import torch
+import tensorflow as tf
 from evolution.gan_train_surr import GanTrain
 # import better_exceptions; better_exceptions.hook()
 import logging
@@ -8,9 +9,12 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 if torch.cuda.is_available():
-    logger.info("CUDA device detected!")
+    logger.info("CUDA device detected for torch!")
 else:
-    logger.info("CUDA device not detected!")
-
+    logger.info("CUDA device not detected for torch!")
+if len(tf.config.list_physical_devices('GPU'))>=1:
+    logger.info("CUDA device detected for TF!")
+else:
+    logger.info("CUDA device not detected for TF!")
 if __name__ == "__main__":
     GanTrain().start()
